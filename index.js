fdd3c53d2bec46e2382b50df4dbfee6c61bec629
sharifulislm <shariful.islam.coder@gmail.com>
2022-01-18 06:50:30 -0800

    first commit

--- index.js
+++ index.js
@@ -19,4 +19,25 @@ if (lastvalue!='')
 resultfield.val(lastvalue.slice(0,-1));
 }
 
+// claudet edn 
+
+
+// this is fun 
+function play() {
+  var audio = new Audio('voice/habiba (1).mp4');
+  audio.play();
+}
+
+function sharif() {
+  var audio = new Audio('voice/shariful.m4a');
+  audio.play();
+}
+
+
+function habiba() {
+  var audio = new Audio('voice/habiba (2).mp4');
+  audio.play();
+}
+
+
 
